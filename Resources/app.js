a12603b1988236e1cd060d04ab46c15d47bed17b
Ralph Crisostomo <rrecrisostomo@gmail.com>
2012-05-05 18:58:29 +0800

    Added: all working files on git!

---
+++ Resources/app.js
@@ -0,0 +1,22 @@
+/*
+ * myWeather
+ * by: Ralph Crisostomo
+ * 18.02.2012
+ * http://ralphcrisostomo.net
+ * 
+ * NEEDS TO DO:
+ * - Transition Animation for Information
+ * - Create a personalize splash screen and icon
+ * - 
+ */
+
+// DECLARE DEPENDENCIES
+var ApplicationWindow	= require('ui/ApplicationWindow');
+
+// Instantiate Application Window
+new ApplicationWindow();
+
+
+Ti.API.info('Hello World');
+
+Ti.API.info('Ralph')
